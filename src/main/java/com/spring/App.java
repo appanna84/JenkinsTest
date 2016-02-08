281a3f1a8f67ccb2a75484680aa4cc06c06af60d
KANGIRA MACHAIAH Appanna (MORPHO) <g513676@mph.morpho.com>
2016-02-08 23:07:23 +0530

    initial

---
+++ src/main/java/com/spring/App.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is hosted in git repository!" );
+    }
+}
